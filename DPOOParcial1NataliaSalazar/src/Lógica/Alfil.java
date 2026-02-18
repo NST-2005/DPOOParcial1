2c42f6e1bbd482609b39cd74fddaa5dc0b19ba1a
macbookair <macbookair@172.20.10.2>
2026-02-18 09:18:57 -0500

    alfil y commit final

--- DPOOParcial1NataliaSalazar/src/Lógica/Alfil.java
+++ DPOOParcial1NataliaSalazar/src/Lógica/Alfil.java
@@ -2,4 +2,17 @@ package Lógica;
 
 public class Alfil extends Piezas{
 
+	public Alfil() {
+		super(valor1, valor2);
+		
+	public void movimientos() {
+		int valor1;
+		int valor2;
+		} 
+			if (Coordenadas.containsKey()== 7 && Coordenadas.containsKey()==1) {
+				desplazamiento(1,1);
+			}
+		}
+
+	}
 }
